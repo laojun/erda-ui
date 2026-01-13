5f7d589ce4b2973a50436c0362e110891340902a
老俊 <782803291@qq.com>
2026-01-13 11:30:14 +0800

    fix: fix version key length

--- shell/app/modules/project/pages/release/components/form.tsx
+++ shell/app/modules/project/pages/release/components/form.tsx
@@ -130,6 +130,7 @@ const ReleaseForm = ({ readyOnly = false }: { readyOnly?: boolean }) => {
       className: 'w-1/2',
       rules: [
         { required: true, message: i18n.t('Please enter the {name}', { name: i18n.t('Version').toLowerCase() }) },
+        { max: 100, message: i18n.t('dop:no more than 100 characters') },
         {
           pattern: /^[A-Za-z0-9._+-]+$/,
           message: i18n.t('dop:Must be composed of letters, numbers, underscores, hyphens and dots.'),
